eef592fb961722223dc89071ce913851efd8ef6d
FFFAN <mankind@FFFAN-IMAC.local>
2026-04-02 01:11:30 +0900

    fix prisma 7 datasource config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,13 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
+  engine: "classic",
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: env("DATABASE_URL"),
   },
 });
